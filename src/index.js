0bd95448ecd41efe8644d2e50f732e104d89ce8d
jamestkelly <jimkelly.t@outlook.com>
2022-05-29 19:29:28 +1000

    Merged all messages, lore, and other flavour text into the lore.json file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+///
 require('dotenv').config();
 const { Client, Intents, MessageEmbed } = require("discord.js");
+const loreBook = require("./resources/lore.json");
 
+/// 
 const bot = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
@@ -9,58 +12,11 @@ const bot = new Client({
     ],
 });
 
+///
 const guildId = process.env.GUILD_ID;
 const spChannelId = process.env.SP_CHANNEL_ID;
-
 const welcomespChannelId = process.env.WELCOME_CHANNEL_ID;
-const welcomeMessage = `Welcome <@?userId>! You're a Morbie now 🧛`;
-
 const requiredRole = process.env.REQUIRED_ROLE;
-
-//TODO: make another file to store these
-//shitty dad jokes
-const jokes = [
-    `I'm afraid for the calendar. Its days are numbered.`,
-    `My wife said I should do lunges to stay in shape. That would be a big step forward.`,
-    `Why do fathers take an extra pair of socks when they go golfing? In case they get a hole in one!`,
-    `Singing in the shower is fun until you get soap in your mouth. Then it's a soap opera.`,
-    `What do a tick and the Eiffel Tower have in common? They're both Paris sites.`,
-    `What do you call a fish wearing a bowtie? Sofishticated.`,
-    `How do you follow Will Smith in the snow? You follow the fresh prints.`,
-    `If April showers bring May flowers, what do May flowers bring? Pilgrims.`,
-    `I thought the dryer was shrinking my clothes. Turns out it was the refrigerator all along.`,
-    `Why are elevator jokes so good? They work on many levels.`
-];
-
-//TODO: replace with short stories + make another file to store these
-//shitty fun facts
-const funFacts = [
-    `avocados are a fruit, not a vegetable`,
-    `the Eiffel Tower can be 15 cm taller during the summer`,
-    `Australia is wider than the moon`,
-    `human teeth are the only part of the body that cannot heal themselves`,
-    `it's illegal to own just one guinea pig in Switzerland`,
-    `the Ancient Romans used to drop a piece of toast into their wine for good health`,
-    `the heart of a shrimp is located in its head`,
-    `baby rabbits are called kits`,
-    `Venus is the only planet to spin clockwise`,
-    'Nutmeg is a hallucinogen'
-];
-
-//TODO: fetch from tenor or giphy OR make another file to store these
-//cute raccoon gifs
-const raccoonGifs = [
-    `https://c.tenor.com/90HLWDj3dQkAAAAd/raccoon-ayasan.gif`,
-    `https://c.tenor.com/Qxf4UdjkrbMAAAAd/raccoon-not-eat-my-grapes.gif`,
-    `https://c.tenor.com/zV97vABOlhcAAAAd/thirsty-hangover.gif`,
-    `https://c.tenor.com/o8UTkIL-_HEAAAAd/bobatyun-raccoon.gif`,
-    `https://c.tenor.com/6WNaY_5A4lwAAAAd/raccoon-raccoon-dance.gif`,
-    `https://c.tenor.com/UtUZtrvzRFkAAAAd/foreign-stance-racoon-racoon-blanket.gif`,
-    `https://c.tenor.com/TS0cA2Y7izgAAAAd/raccoon-racon.gif`,
-    `https://c.tenor.com/JhAkD080yYAAAAAd/scooch-raccoon.gif`,
-    `https://c.tenor.com/W1IGDx7Qo2cAAAAd/racoon-raccoon.gif`,
-    `https://c.tenor.com/8LxqBe2hS6YAAAAd/raccoon-spin.gif`
-];
 
 bot
     .login(
@@ -73,7 +29,7 @@ bot
         });
     });
 
-
+///
 bot.on("ready", () => {
     const guild = bot.guilds.cache.get(guildId);
     console.log(`Bot status: ONLINE (Serving '${guild.name}' | ${guild.id} | Bot name: ${bot.user.tag})`);
@@ -105,7 +61,7 @@ bot.on("ready", () => {
                 { name: 'Inline field title', value: 'Some value here', inline: true },
             )*/
             //.addField('Inline field title', 'Some value here', true)
-            .setImage(raccoonGifs[Math.floor(Math.random() * raccoonGifs.length)])
+            .setImage(loreBook.raccoonGifs[Math.floor(Math.random() * loreBook.raccoonGifs.length)])
             .setTimestamp()
         //.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
@@ -129,7 +85,7 @@ bot.on("ready", () => {
                 //Ensure a bot isn't selected, and only people with the required role are considered
                 let randomUser = members.filter(member => !member.user.bot && member._roles.includes(requiredRole)).randomKey();
                 guild.channels.fetch(spChannelId).then((channel) => {
-                    channel.send(`<@${randomUser}> Did you know ${funFacts[Math.floor(Math.random() * funFacts.length)]}?`);
+                    channel.send(`<@${randomUser}> Did you know ${loreBook.funFacts[Math.floor(Math.random() * loreBook.funFacts.length)]}?`);
                     logDate('Random fact sent');
                 });
             })
@@ -139,16 +95,18 @@ bot.on("ready", () => {
 
 
 //Welcome message
+///
 bot.on("guildMemberAdd", (member) => {
     member.guild.channels.fetch(welcomespChannelId).then((channel) => {
-        channel.send(welcomeMessage.replace('?userId', member.id));
-        //TODO: uncomment below when out of testing and global role added to everyone
-        //member.roles.add(requiredRole);
-        //logDate("Required role added to new member")
+        channel.send(loreBook.welcomeMessage.replace('?userId', member.id));
+        // TODO: Uncomment below when out of testing and global role added to everyone
+        // member.roles.add(requiredRole);
+        // logDate("Required role added to new member")
     });
 });
 
 //Message reactions / commands
+///
 bot.on("messageCreate", (message) => {
     if (message.author.bot) {
         return;
@@ -163,7 +121,7 @@ bot.on("messageCreate", (message) => {
         message.react('🇺');
         message.react('🇸');
         message.react('🦇');
-        logDate('Reacted to a morbius mention')
+        logDate('Reacted to a morbius mention');
     } else if (message.content == "$help") {
         const commands = new MessageEmbed()
             .setColor("#ffd046")
@@ -174,23 +132,25 @@ bot.on("messageCreate", (message) => {
                 { name: "$joke", value: "Sends a random joke" }
             );
         message.channel.send({ embeds: [commands] });
-        logDate('Replied to $help')
+        logDate('Replied to $help');
     } else if (message.content == "$random") {
         message.react("🔢");
         let randomNumber = getRandomNumber(1, 100);
         message.reply(`Your random number is ${randomNumber}.`);
-        logDate('Replied to $random command')
+        logDate('Replied to $random command');
     } else if (message.content == '$joke') {
-        message.channel.send(jokes[Math.floor(Math.random() * jokes.length)]);
-        logDate('Replied to $joke command')
+        message.channel.send(loreBook.jokes[Math.floor(Math.random() * loreBook.jokes.length)]);
+        logDate('Replied to $joke command');
     }
 });
 
+///
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 };
 
+///
 function logDate(message) {
     let date = new Date().toLocaleString('en-AU');
-    console.log(`[${date.replace(',', '')}] ${message.toUpperCase()}`)
+    console.log(`[${date.replace(',', '')}] ${message.toUpperCase()}`);
 }
